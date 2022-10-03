4852d29b4b3aa7946d62c65acf42013c245c5820
HeilRulez <rulez.heil@gmail.com>
2022-10-04 02:03:15 +0600

    не меняется стэйт

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const configApi = {
-    baseUrl: 'http://localhost:3000',
-    // baseUrl: 'https://api.tvoyomesto.nomorepartiesxyz.ru',
+    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://api.tvoyomesto.nomorepartiesxyz.ru',
     type: 'application/json',
   }
